97235a7ede8d5e35627671335777000e32f6225e
Emmanuel Martinez <hemmanuelmtz777@gmail.com>
2025-11-12 17:07:31 -0600

    Refactor server setup and route handling

--- index.js
+++ index.js
@@ -4,16 +4,19 @@ import productRoutes from "./Routes/ProjectRoutes.js";
 import vehicleRoutes from "./Routes/VehicleRoutes.js";
 import AccessRouter  from "./Routes/AccessRouter.js";
 import dotenv from 'dotenv';
-dotenv.config();
 
+dotenv.config();
+const Port= process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
+app.get('/', (req, res) => {
+    res.send('Projects endpoint works');
+});
+app.use('/users', userRoutes);
+app.use('/projects', productRoutes);
+app.use('/vehicles', vehicleRoutes);
+app.use('/access', AccessRouter);
 
-app.use('/api/users', userRoutes);
-app.use('/api/projects', productRoutes);
-app.use('/api/vehicles', vehicleRoutes);
-app.use('/api/access', AccessRouter);
-
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on  http://localhost:${process.env.PORT}`);
+app.listen(Port, () => {
+    console.log(`Server is running on  http://localhost:${Port}`);
 });
