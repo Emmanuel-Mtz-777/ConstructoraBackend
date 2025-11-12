9beae063b3e917cd37567eb149a5766c217d2332
Leviek <hemmanuelmtz777@gmail.com>
2025-11-12 16:48:20 -0600

    Upload to heroku

--- Contracts/Mongo.js
+++ Contracts/Mongo.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
-import { DB_USER,DB_PASSWORD } from '../config.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const uri =`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.oyy1w7r.mongodb.net/?appName=Cluster0`;
+const uri =`mongodb+srv://${process.env.MONGO_User}:${process.env.MONGO_PASSWORD}@cluster0.oyy1w7r.mongodb.net/?appName=Cluster0`;
 
 mongoose.connect(uri)
     .then(() => console.log('MongoDB connected'))
