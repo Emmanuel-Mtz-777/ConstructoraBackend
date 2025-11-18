c2d1336eec10606fab81dc2d2b8513636d952cb2
Leviek <hemmanuelmtz777@gmail.com>
2025-11-18 09:38:20 -0600

    correction in api

--- index.js
+++ index.js
@@ -26,9 +26,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 
-app.use('/',(req,res)=>{
-  return res.send('Welcome to the API');
-});
+app.get('/', (req,res) => res.send('Welcome to the API'));
+
 
 app.use('/users', createUserRouter({ UserModel }));
 app.use(generalRateLimit);
